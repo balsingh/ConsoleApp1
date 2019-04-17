1e77c25ec6396b617c3e4ef2842f99071f2e8360
Balbir Singh <Balsingh@hotmail.co.uk>
2019-04-17 16:38:36 +0000

    initial

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using k8s;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = KubernetesClientConfiguration.InClusterConfig();
+            // Use the config object to create a client.
+            
+            IKubernetes client = new Kubernetes(config);
+            Console.WriteLine("Starting Request!");
+
+            var list = client.ListNamespacedPod("default");
+            foreach (var item in list.Items)
+            {
+                Console.WriteLine(item.Metadata.Name);
+            }
+            if (list.Items.Count == 0)
+            {
+                Console.WriteLine("Empty!");
+            }
+
+        }
+    }
+}
